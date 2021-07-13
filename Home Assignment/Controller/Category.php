fb7c522ec73d5813cf845fa0b4b5c0bd6066ad51
Shahed Islam <shahedislam600@gmail.com>
2021-07-13 06:05:13 +0600

    Home Assignment

---
+++ Home Assignment/Controller/Category.php
@@ -0,0 +1,50 @@
+<?php  
+     include 'Model/db_config.php';
+
+     $name="";
+     $err_name="";
+     $err_db="";
+
+     $hasError = false;
+
+     if(isset($_POST["Submit"]))
+     {
+     	if(empty($_POST["text"]))
+     	{
+     		$err_name="Name Required";
+            $hasError = true;
+     	}
+
+     	else
+     	{
+     		$name=$_POST["text"];
+     	}
+
+     	//Database
+
+     	if(!$hasError)
+     	{
+     		$rs= insertCatagory("$name");
+     		var_dump($rs);
+     		/*if($rs===true)
+     		{
+     			header("Location: AllCatagories.php");
+     		}*/
+
+     	}
+     }
+
+     
+     function insertCatagory($name)
+     {
+     	$query="INSERT INTO catagory VALUES(NULL,'$name')";
+     	return execute($query);
+     }
+
+     function allCatagories()
+     {
+     	$query= "select * from catagory";
+     	return $rs= get($query);
+     }
+
+?>
